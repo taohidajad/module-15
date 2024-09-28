317a6ea754ef8641ac7e791186432c483d0ae194
Taohid <taohidajadpro@gmail.com>
2024-09-28 21:43:30 +0600

    Your commit message

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{ function index($id) {
+   $name = 'Donal Trump';
+   $age = 75;
+
+   $data = [
+       'id' => $id,
+       'name' => $name,
+       'age' => $age
+   ];
+
+   $cookie_name='access_token';
+   $cookie_value='123-XYZ';
+   $minutes = 1;
+   $path = '/';
+   $domain =$_SERVER['SERVER_NAME'];
+   $secure = false;
+   $httpOnly = true;
+
+   $cookie=cookie($cookie_name, $cookie_value, $minutes, $path, $domain, $secure, $httpOnly);
+   return response()->json($data, 200)->cookie($cookie);
+   
+}
+}
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',function(){
+    return view('welcome');
+});
+
+Route::get('/profile/{id}',[ProfileController::class,'index']);
